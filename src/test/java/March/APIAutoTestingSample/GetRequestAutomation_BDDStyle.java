218c4cd98603717d14d21efc20d843b3e1090d37
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2025-03-07 13:40:33 +0530

    7th March Changes

--- src/test/java/March/APIAutoTestingSample/GetRequestAutomation_BDDStyle.java
+++ src/test/java/March/APIAutoTestingSample/GetRequestAutomation_BDDStyle.java
@@ -3,7 +3,7 @@ package March.APIAutoTestingSample;
 import io.restassured.RestAssured;
 
 
-public class GetRequestAutomation {
+public class GetRequestAutomation_BDDStyle {
 
     public static void main(String[] args) {
 
