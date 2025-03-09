00176e53f3e69d2d796c1af45fd7ec4b47404f8b
LITERAMS\shubham.sharma <Shubham.sharma@litera.com>
2025-03-09 22:22:40 +0530

    9th March Changes

---
+++ src/test/java/DELRequest/DeleteReq.java
@@ -0,0 +1,42 @@
+package DELRequest;
+
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.Test;
+
+public class DeleteReq {
+
+
+    RequestSpecification r;
+    Response res;
+    ValidatableResponse validatableResponse;
+
+    @Description("Verify Deleting of the Booking")
+    @Test
+    public void restFullBooker_DeleteReq_Positive(){
+
+        String bookingId="50";
+        final String TOKEN="Basic YWRtaW46cGFzc3dvcmQxMjM=";
+
+        // First Part Preconditions
+        r= RestAssured.given();
+        r.baseUri("https://restful-booker.herokuapp.com");
+        r.basePath("booking/"+bookingId);
+        r.contentType(ContentType.JSON);
+        r.header("Authorization",TOKEN);
+
+
+        //Second Part -- Making request
+        res=r.log().all().when().delete();
+
+        //Third part validating response
+        validatableResponse= res.then().log().all();
+        validatableResponse.statusCode(201);
+
+
+    }
+}
--- src/test/java/PostRequest/PostRequest.java
+++ src/test/java/PostRequest/PostRequest.java
@@ -3,12 +3,16 @@ package PostRequest;
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.testng.annotations.Test;
 
 public class PostRequest {
 
-    RequestSpecification r= RestAssured.given();
+    RequestSpecification r;
+    Response response;
+    ValidatableResponse vr;
 
 
     // Non-BDD style test case for POST request
@@ -16,18 +20,26 @@ public class PostRequest {
     @Test
     public void restFullBooker_POSTReq_Positive(){
 
+
+
         String payload="{\n" +
                 "    \"username\" : \"admin\",\n" +
                 "    \"password\" : \"password123\"\n" +
                 "}";
 
-        r.given();
+        // Preconditions
+        r=RestAssured.given();
         r.baseUri("https://restful-booker.herokuapp.com");
         r.basePath("/auth");
         r.contentType(ContentType.JSON);
         r.body(payload).log().all();
-        r.when().log().all().post();
-        r.then().log().all().statusCode(200);
+
+        // Making a API POST req Part 2
+        response=r.when().log().all().post();
+
+        // Verification -Part 3
+        vr=response.then().log().all();
+        vr.statusCode(200);
     }
 
 }
